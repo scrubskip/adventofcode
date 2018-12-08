309621acf6746d345abae6293ff6971a640ed597
Justin Koh <kip@kipster.org>
2018-12-07 21:59:14 -0800

    Day 6 picking up again

--- day6/largest_area.py
+++ day6/largest_area.py
@@ -6,16 +6,34 @@ from parse import compile
 
 def main():
     points_file = open("day6input.txt", "r")
-    p = compile("{x:d}, {y:d}")
-    points = map(p.parse, points_file.readlines())
-    #print points
-    max_x = max(points, key=lambda point: point['x'])['x']
-    min_x = min(points, key=lambda point: point['x'])['x']
-    max_y = max(points, key=lambda point: point['y'])['y']
-    min_y = min(points, key=lambda point: point['y'])['y']
 
-    print "Range: [{0},{1}] to [{2},{3}]".format(min_x, min_y, max_x, max_y)
-    # If any points are on the edges, throw them out.
+    points = parse_points(points_file.readlines())
+
+
+def parse_points(point_strings):
+    p = compile("{x:d}, {y:d}")
+    return_list = []
+    for point_str in point_strings:
+        data = p.parse(point_str)
+        if (data):
+            return_list.append(Point(int(data['x']), int(data['y'])))
+
+    return return_list
+
+
+def get_visible_range(points):
+    max_x = max(points, key=lambda point: point.x).x
+    min_x = min(points, key=lambda point: point.x).x
+    max_y = max(points, key=lambda point: point.y).y
+    min_y = min(points, key=lambda point: point.y).y
+
+    return [Point(min_x, min_y), Point(max_x, max_y)]
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 
 if __name__ == '__main__':
--- day6/largest_area_test.py
+++ day6/largest_area_test.py
@@ -1,10 +1,21 @@
 import largest_area
+from largest_area import Point
 import unittest
 
 
 class LargestAreaTestCase(unittest.TestCase):
-    def test_analyze(self):
-        self.assertEquals(True, True)
+    def test_visible_range(self):
+        points = [
+            Point(5, 0),
+            Point(10, 1),
+            Point(25, 10)
+        ]
+
+        vrange = largest_area.get_visible_range(points)
+        self.assertEquals(5, vrange[0].x)
+        self.assertEquals(0, vrange[0].y)
+        self.assertEquals(25, vrange[1].x)
+        self.assertEquals(10, vrange[1].y)
 
 
 if __name__ == '__main__':
