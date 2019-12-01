6d04eb83fadd86d17c33a31b57e69cf2d6525026
Justin Koh <kip@kipster.org>
2019-12-01 13:26:47 -0800

    Day 1.2

--- 2019/day1/fuel.py
+++ 2019/day1/fuel.py
@@ -6,13 +6,22 @@ def main():
     modules = open("day1input.txt", "r")
     requirement_sum = 0
     for module in modules.readlines():
-        requirement_sum += get_fuel_requirement(module)
+        requirement_sum += get_fuel_requirement_inclusive(module)
 
     return requirement_sum
 
 
 def get_fuel_requirement(mass):
     return int(int(mass) / 3) - 2
+
+
+def get_fuel_requirement_inclusive(mass):
+    total = 0
+    fuel = get_fuel_requirement(mass)
+    while (fuel > 0):
+        total += fuel
+        fuel = get_fuel_requirement(fuel)
+    return total
 
 
 if __name__ == "__main__":
--- 2019/day1/fuel_test.py
+++ 2019/day1/fuel_test.py
@@ -9,6 +9,11 @@ class FuelTestCase(unittest.TestCase):
         self.assertEqual(654, fuel.get_fuel_requirement(1969))
         self.assertEqual(33583, fuel.get_fuel_requirement(100756))
 
+    def test_recursive(self):
+        self.assertEquals(2, fuel.get_fuel_requirement_inclusive(14))
+        self.assertEqual(966, fuel.get_fuel_requirement_inclusive(1969))
+        self.assertEqual(50346, fuel.get_fuel_requirement_inclusive(100756))
+
 
 if __name__ == '__main__':
     unittest.main()
