d1be3401affcf912905ac4f737fc9fde328d49e3
Justin Koh <justinkoh@google.com>
2019-12-03 13:47:52 -0800

    Day 2.2 done.

--- 2019/day2/opcode.py
+++ 2019/day2/opcode.py
@@ -6,15 +6,30 @@ def main():
     opcode_data = open("day2input.txt", "r").readline()
     opcode_data = map(int, opcode_data.split(","))
 
-    # Edits from program
-    opcode_data[1] = 12
-    opcode_data[2] = 2
+    #opcode_data = run(opcode_data, noun = 12, verb = 2)
+    #return opcode_data[0]
 
-    opcode_data = run(opcode_data)
-    return opcode_data[0]
 
-def run(data):
+    for noun_arg in range(100):
+        for verb_arg in range(100):
+            test_data = run(opcode_data, noun = noun_arg, verb = verb_arg)
+            if (test_data[0] == 19690720):
+                print "Found at ", noun_arg, ", ", verb_arg
+                return
+
+    print "Not found"
+
+def run(data, *args, **kwargs):
     program_counter = 0
+    data = list(data)
+
+    noun = kwargs.get("noun", None)
+    verb = kwargs.get("verb", None)
+
+    if (noun):
+        data[1] = noun
+    if (verb):
+        data[2] = verb
     
     while (True):
         opcode = data[program_counter]
@@ -25,7 +40,7 @@ def run(data):
         output_pos = data[program_counter + 3]
         output = 0
 
-        print [opcode, input_1, input_2, output_pos]
+        #print [opcode, input_1, input_2, output_pos]
 
         if (opcode == 1):
             output = input_1 + input_2
@@ -34,7 +49,7 @@ def run(data):
         else:
             raise AssertionError("invalid opcode")
 
-        print(data)
+        #print(data)
 
         data[output_pos] = output
         program_counter += 4
