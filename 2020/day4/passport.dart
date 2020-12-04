f67a58ddb6c6e65e58b6bcc742c70cbe1c9a1f5d
Justin Koh <kip@kipster.org>
2020-12-04 14:48:09 -0800

    Day4.2 works.

--- 2020/day4/passport.dart
+++ 2020/day4/passport.dart
@@ -13,10 +13,9 @@ void main(List<String> args) {
           .openRead()
           .transform(utf8.decoder)
           .transform(new LineSplitter()))
-      .where(isValid)
-      .toList()
-      .then((List<Map<String, String>> results) =>
-          stdout.writeln(results.length));
+      .where(isStrictlyValid)
+      .length
+      .then((result) => stdout.writeln(result));
 }
 
 Stream<String> getListAsStream(List<String> input) async* {
@@ -56,9 +55,73 @@ final List<String> REQUIRED_FIELDS = [
   "pid"
 ];
 
+final RegExp HAIR_COLOR_MATCHER = new RegExp(r"#[a-f0-9]{6}");
+
+final RegExp PID_MATCHER = new RegExp(r"[0-9]{9}");
+
+final Set<String> VALID_EYE_COLORS =
+    Set.of(["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]);
+
 bool isValid(Map<String, String> passport) {
   return REQUIRED_FIELDS
           .where((element) => passport.containsKey(element))
           .length ==
       REQUIRED_FIELDS.length;
+}
+
+bool isStrictlyValid(Map<String, String> passport) {
+  return REQUIRED_FIELDS
+          .where((element) => checkField(passport[element], element))
+          .length ==
+      REQUIRED_FIELDS.length;
+}
+
+/**
+ * Rules:
+ * byr (Birth Year) - four digits; at least 1920 and at most 2002.
+ * iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+ * eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+ * hgt (Height) - a number followed by either cm or in:
+ * If cm, the number must be at least 150 and at most 193.
+ * If in, the number must be at least 59 and at most 76.
+ * hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+ * ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+ * pid (Passport ID) - a nine-digit number, including leading zeroes.
+ * cid (Country ID) - ignored, missing or not.
+ */
+
+bool checkField(String value, String key) {
+  if (value == null) {
+    return false;
+  }
+  switch (key) {
+    case "byr":
+      int byr = int.tryParse(value);
+      return byr >= 1920 && byr <= 2002;
+    case "iyr":
+      int iyr = int.tryParse(value);
+      return iyr >= 2010 && iyr <= 2020;
+    case "eyr":
+      int eyr = int.tryParse(value);
+      return eyr >= 2020 && eyr <= 2030;
+    case "hgt":
+      if (value.length < 3) {
+        return false;
+      }
+      int hgt = int.tryParse(value.substring(0, value.length - 2));
+      if (value.endsWith("cm")) {
+        return hgt >= 150 && hgt <= 193;
+      } else if (value.endsWith("in")) {
+        return hgt >= 59 && hgt <= 76;
+      }
+      return false;
+    case "hcl":
+      return HAIR_COLOR_MATCHER.hasMatch(value);
+    case "ecl":
+      return VALID_EYE_COLORS.contains(value);
+    case "pid":
+      return value.length == 9 && PID_MATCHER.hasMatch(value);
+    default:
+      return false;
+  }
 }
--- 2020/day4/passport_test.dart
+++ 2020/day4/passport_test.dart
@@ -4,6 +4,9 @@ import "passport.dart";
 
 void main() {
   test("Aoc input", _testAocInput);
+  test("strict atoms", _testStrictAtoms);
+  test("strict invalid", _testStrictlyValidInvalid);
+  test("strict valid", _testStrictlyValid);
 }
 
 void _testAocInput() async {
@@ -29,4 +32,77 @@ void _testAocInput() async {
   expect(output[1]['iyr'], "2013");
 
   expect(output.where((element) => isValid(element)).length, 2);
+}
+
+void _testStrictAtoms() {
+  expect(checkField("2002", "byr"), true);
+  expect(checkField("2003", "byr"), false);
+
+  expect(checkField("60in", "hgt"), true);
+  expect(checkField("190cm", "hgt"), true);
+  expect(checkField("190in", "hgt"), false);
+  expect(checkField("190", "hgt"), false);
+
+  expect(checkField("#123abc", "hcl"), true);
+  expect(checkField("#123abz", "hcl"), false);
+  expect(checkField("123abc", "hcl"), false);
+
+  expect(checkField("brn", "ecl"), true);
+  expect(checkField("wat", "ecl"), false);
+
+  expect(checkField("000000001", "pid"), true);
+  expect(checkField("0123456789", "pid"), false);
+}
+
+void _testStrictlyValidInvalid() async {
+  var invalidData = [
+    "eyr:1972 cid:100",
+    "hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
+    "",
+    "iyr:2019",
+    "hcl:#602927 eyr:1967 hgt:170cm",
+    "ecl:grn pid:012533040 byr:1946",
+    "",
+    "hcl:dab227 iyr:2012",
+    "ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277",
+    "",
+    "hgt:59cm ecl:zzz",
+    "eyr:2038 hcl:74454a iyr:2023",
+    "pid:3556412378 byr:2007"
+  ];
+
+  Stream<Map<String, String>> stream =
+      getPassportStream(getListAsStream(invalidData));
+  List<Map<String, String>> output = await stream.toList();
+
+  expect(output[0]['eyr'], "1972");
+  expect(output[1]['iyr'], "2019");
+
+  expect(output.where((element) => isStrictlyValid(element)).length, 0);
+}
+
+void _testStrictlyValid() async {
+  var validData = [
+    "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980",
+    "hcl:#623a2f",
+    "",
+    "eyr:2029 ecl:blu cid:129 byr:1989",
+    "iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
+    "",
+    "hcl:#888785",
+    "hgt:164cm byr:2001 iyr:2015 cid:88",
+    "pid:545766238 ecl:hzl",
+    "eyr:2022",
+    "",
+    "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"
+  ];
+
+  Stream<Map<String, String>> stream =
+      getPassportStream(getListAsStream(validData));
+  List<Map<String, String>> output = await stream.toList();
+
+  expect(output[0]['eyr'], "2030");
+  expect(output[1]['iyr'], "2014");
+
+  expect(output.where((element) => isStrictlyValid(element)).length, 4);
 }
