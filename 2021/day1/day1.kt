9f6db9597d4e82f4af265ee1e6adc910048626cb
Justin Koh <kip@kipster.org>
2021-12-01 19:20:38 -0800

    Added fold version.

--- 2021/day1/day1.kt
+++ 2021/day1/day1.kt
@@ -3,6 +3,9 @@ import java.io.File
 fun main() {
     // println(findIncreasesSlidingWindow(listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263),
     // 3))
+
+    println(findIncreasesFold(listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)))
+
     val lineList = mutableListOf<Int>()
 
     File("day1.txt").useLines { lines -> lines.forEach { lineList.add(it.toInt()) } }
@@ -19,6 +22,12 @@ fun findIncreases(input: List<Int>): Int {
         prevValue = value
     }
     return increases
+}
+
+fun findIncreasesFold(input: List<Int>): Int {
+    return input.foldIndexed(0) { index, increases, value ->
+        if (index > 0 && value > input[index - 1]) increases + 1 else increases
+    }
 }
 
 fun findIncreasesSlidingWindow(input: List<Int>, windowSize: Int): Int {
