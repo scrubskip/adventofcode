b26f4ca1f116222fd81e10d004bb556dce9c42ee
Justin Koh <kip@kipster.org>
2021-12-01 23:25:13 -0800

    Day 2.2: added aim.

--- 2021/day2/day2.kt
+++ 2021/day2/day2.kt
@@ -8,14 +8,16 @@ fun main() {
 
 fun testSample(): Int {
     val (depth, distance) =
-            moveSub(listOf("forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"))
+            moveSubWithAim(
+                    listOf("forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2")
+            )
     return depth * distance
 }
 
 fun readFile(arg: String): Int {
     val lineList = mutableListOf<String>()
     File(arg).useLines { lines -> lines.forEach { lineList.add(it.toString()) } }
-    val (depth, distance) = moveSub(lineList)
+    val (depth, distance) = moveSubWithAim(lineList)
     return depth * distance
 }
 
@@ -32,6 +34,29 @@ fun moveSub(input: List<String>): Pair<Int, Int> {
             "forward" -> distance += unit
             "down" -> depth += unit
             "up" -> depth -= unit
+        }
+    }
+
+    return Pair(depth, distance)
+}
+
+fun moveSubWithAim(input: List<String>): Pair<Int, Int> {
+    var depth: Int = 0
+    var distance: Int = 0
+    var aim: Int = 0
+
+    input.forEach {
+        // println(it)
+        val command = it.split(" ")
+        val direction = command[0]
+        val unit: Int = command[1].toInt()
+        when (direction) {
+            "forward" -> {
+                distance += unit
+                depth += (aim * unit)
+            }
+            "down" -> aim += unit
+            "up" -> aim -= unit
         }
     }
 
