998f2e01879079cbc9cf320de300ec0268e928e4
Justin Koh <kip@kipster.org>
2021-12-11 20:44:05 -0800

    Day 11.1 done.

---
+++ 2021/src/day11/Day11KtTest.kt
@@ -0,0 +1,65 @@
+package day11
+
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+
+
+internal class Day11KtTest {
+    @Test
+    fun testNeighbors() {
+        assertEquals(3, getNeighbors(Pair(0, 0), 10, 10).size)
+        assertEquals(5, getNeighbors(Pair(0, 1), 10, 10).size)
+        assertEquals(5, getNeighbors(Pair(1, 0), 10, 10).size)
+        assertEquals(8, getNeighbors(Pair(1, 1), 10, 10).size)
+
+        assertEquals(5, getNeighbors(Pair(0, 5), 10, 10).size)
+
+        assertEquals(3, getNeighbors(Pair(9, 9), 10, 10).size)
+        assertEquals(5, getNeighbors(Pair(9, 8), 10, 10).size)
+        assertEquals(5, getNeighbors(Pair(8, 9), 10, 10).size)
+        assertEquals(8, getNeighbors(Pair(8, 8), 10, 10).size)
+    }
+
+    @Test
+    fun testSimpleExample() {
+        val input = listOf(
+            "11111",
+            "19991",
+            "19191",
+            "19991",
+            "11111"
+        )
+        val octoBoard = parseData(input)
+        // println(octoBoard)
+
+        val flashed = octoBoard.runStep()
+        // println(octoBoard)
+        assertEquals(9, flashed)
+    }
+
+    @Test
+    fun testExample() {
+        val input = listOf(
+            "5483143223",
+            "2745854711",
+            "5264556173",
+            "6141336146",
+            "6357385478",
+            "4167524645",
+            "2176841721",
+            "6882881134",
+            "4846848554",
+            "5283751526"
+        )
+
+        val octoBoard = parseData(input)
+        // println("$octoBoard\n")
+        var sumFlash = 0
+        for (step in 1..100) {
+            sumFlash += octoBoard.runStep()
+            // println("$octoBoard\n")
+        }
+        assertEquals(1656, sumFlash)
+    }
+}
---
+++ 2021/src/day11/day11.kt
@@ -0,0 +1,107 @@
+package day11
+
+import java.io.File
+import java.lang.Integer.max
+import java.lang.Integer.min
+
+fun main() {
+    val octoBoard = parseData(File("src/day11", "day11input.txt").readLines())
+    var sumFlashes = 0
+    for (i in 1..100) {
+        sumFlashes += octoBoard.runStep()
+    }
+    println("$sumFlashes")
+}
+
+fun parseData(input: List<String>): OctoBoard {
+    return OctoBoard(input.map { it.toCharArray().map { digit -> digit.digitToInt() }.toIntArray() }.toTypedArray())
+}
+
+data class OctoBoard(val boardData: Array<IntArray>) {
+
+    /**
+     * Runs a step.
+     * @return Number of flashed cells
+     */
+    fun runStep(): Int {
+        var cellsToFlash = ArrayDeque<Pair<Int, Int>>()
+        var flashedCells = mutableSetOf<Pair<Int, Int>>()
+        // First add 1 to all points
+        var numCols: Int = 0
+        boardData.forEachIndexed { rowIndex, row ->
+            row.forEachIndexed { colIndex, value ->
+                boardData[rowIndex][colIndex] = value + 1
+                if (boardData[rowIndex][colIndex] > 9) {
+                    cellsToFlash.add(Pair(rowIndex, colIndex))
+                }
+                if (colIndex > numCols) {
+                    numCols = colIndex
+                }
+            }
+        }
+        while (cellsToFlash.isNotEmpty()) {
+            flashCell(cellsToFlash.removeFirst(), numCols + 1, cellsToFlash, flashedCells)
+        }
+        // Now take all flashed cells and set to 0
+        flashedCells.forEach {
+            boardData[it.first][it.second] = 0
+        }
+        return flashedCells.size
+    }
+
+    private fun flashCell(
+        cell: Pair<Int, Int>,
+        numCols: Int,
+        cellsToFlash: ArrayDeque<Pair<Int, Int>>,
+        flashedCells: MutableSet<Pair<Int, Int>>
+    ) {
+        if (flashedCells.contains(cell)) {
+            return
+        }
+        flashedCells.add(cell)
+        // Add 1 to all the neighbors
+        val neighbors = getNeighbors(cell, boardData.size, numCols)
+        for (neighbor in neighbors) {
+            boardData[neighbor.first][neighbor.second] += 1
+        }
+        for (neighbor in neighbors) {
+            if (boardData[neighbor.first][neighbor.second] > 9) {
+                cellsToFlash.add(neighbor)
+            }
+        }
+    }
+
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as OctoBoard
+
+        if (!boardData.contentDeepEquals(other.boardData)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return boardData.contentDeepHashCode()
+    }
+
+    override fun toString(): String {
+        return boardData.map {
+            it.joinToString()
+        }.joinToString(separator = "\n")
+    }
+}
+
+fun getNeighbors(cell: Pair<Int, Int>, numRows: Int, numCols: Int): List<Pair<Int, Int>> {
+    return buildList {
+        for (row in (max(0, cell.first - 1)..min(cell.first + 1, numRows - 1))) {
+            for (col in (max(0, cell.second - 1)..min(cell.second + 1, numCols - 1))) {
+                if (row != cell.first || col != cell.second) {
+                    add(Pair(row, col))
+                }
+            }
+        }
+    }
+}
