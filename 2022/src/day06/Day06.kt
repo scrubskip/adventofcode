8596ed986fbc4989a4216f40e8383466d41f8042
Justin Koh <kip@kipster.org>
2022-12-05 23:13:59 -0800

    Day 6.2 done.

--- 2022/src/day06/Day06.kt
+++ 2022/src/day06/Day06.kt
@@ -5,16 +5,25 @@ import java.io.File
 fun main() {
     val input = File("src/day06/day06input.txt").readLines().first()
     println(getFirstMarkerIndex(input))
+    println(getMessageIndex(input))
 }
 
 fun getFirstMarkerIndex(input: String): Int {
-    if (input.length < 4) {
+    return getDistinctChars(input, 4)
+}
+
+fun getMessageIndex(input: String): Int {
+    return getDistinctChars(input, 14)
+}
+
+fun getDistinctChars(input: String, length: Int): Int {
+    if (input.length < length) {
         throw Exception("Input is too short")
     }
 
-    for (index in 0..input.length - 4) {
-        if (input.substring(index, index + 4).toSet().size == 4) {
-            return index + 4
+    for (index in 0..input.length - length) {
+        if (input.substring(index, index + length).toSet().size == length) {
+            return index + length
         }
     }
     return -1
--- 2022/src/day06/Day06KtTest.kt
+++ 2022/src/day06/Day06KtTest.kt
@@ -11,7 +11,14 @@ internal class Day06KtTest {
         assertEquals(6, getFirstMarkerIndex("nppdvjthqldpwncqszvftbrmjlhg"))
         assertEquals(10, getFirstMarkerIndex("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
         assertEquals(11, getFirstMarkerIndex("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
+    }
 
-
+    @Test
+    fun testSampleMessage() {
+        assertEquals(19, getMessageIndex("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
+        assertEquals(23, getMessageIndex("bvwbjplbgvbhsrlpgdmjqwftvncz"))
+        assertEquals(23, getMessageIndex("nppdvjthqldpwncqszvftbrmjlhg"))
+        assertEquals(29, getMessageIndex("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
+        assertEquals(26, getMessageIndex("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
     }
 }
