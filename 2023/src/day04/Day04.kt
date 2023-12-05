2b8e1924f3eaf7739ee3b53a193cc9348f367b2d
Justin Koh <kip@kipster.org>
2023-12-04 17:27:44 -0800

    Day 4.2 done.
    Also updated part 1 a bit.

--- 2023/src/day04/Day04.kt
+++ 2023/src/day04/Day04.kt
@@ -5,17 +5,60 @@ import kotlin.math.pow
 
 fun main() {
     val input = File("src/day04/Day04.txt").readLines()
-    val scratchers = input.map { Scratcher(it) }
+    val scratchers = Pile(input)
 
-    println(scratchers.sumOf { it.getPointValue() })
+    println(scratchers.getPointValue())
+    println(scratchers.getCardCount())
 }
 
 val SPACE = Regex("\\s+")
 
+class Pile(input: List<String>) {
+    private val cards: Map<Int, Scratcher>
+
+    init {
+        cards = buildMap {
+            input.forEach {
+                val scratcher = Scratcher(it)
+                put(scratcher.id, scratcher)
+            }
+        }
+    }
+
+    fun getCardCount(): Int {
+        val countMap = mutableMapOf<Int, Int>()
+
+        cards.values.forEach {
+            addScratcher(it, countMap)
+        }
+        return countMap.values.sum()
+    }
+
+    fun getPointValue(): Long {
+        return cards.values.sumOf { it.getPointValue() }
+    }
+
+    private fun addScratcher(scratcher: Scratcher, countMap: MutableMap<Int, Int>) {
+        val id = scratcher.id
+        // Add to map
+        // println("adding $id")
+
+        countMap.putIfAbsent(id, 0)
+        countMap[id] = countMap[id]!! + 1
+        (id + 1).rangeTo(id + scratcher.getWinCount()).forEach {
+            cards[it]?.let { newScratcher ->
+                addScratcher(newScratcher, countMap)
+            }
+        }
+    }
+}
+
 class Scratcher(input: String) {
     private val winningNumbers: MutableSet<Long> = mutableSetOf()
     private val numbers: MutableSet<Long> = mutableSetOf()
+    private var pointValue: Long? = null
     val id: Int
+
 
     init {
         id = input.substring(0, input.indexOf(":")).split(SPACE)[1].toInt()
@@ -24,12 +67,19 @@ class Scratcher(input: String) {
         numbers.addAll(numberStrings[1].split(SPACE).map { it.toLong() })
     }
 
+    fun getWinCount(): Int {
+        return numbers.count { winningNumbers.contains(it) }
+    }
+
     fun getPointValue(): Long {
-        val count = numbers.count { winningNumbers.contains(it) }
-        return if (count == 0) {
-            0L
-        } else {
-            2.0.pow(numbers.count { winningNumbers.contains(it) } - 1).toLong()
+        if (pointValue == null) {
+            val count = getWinCount()
+            pointValue = if (count == 0) {
+                0L
+            } else {
+                2.0.pow(count - 1).toLong()
+            }
         }
+        return pointValue as Long
     }
 }
