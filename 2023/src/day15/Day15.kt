43f749277df6394c4be735f13f35d58902de56dd
Justin Koh <kip@kipster.org>
2023-12-16 13:47:43 -0800

    Day 15.2 done.

--- 2023/src/day15/Day15.kt
+++ 2023/src/day15/Day15.kt
@@ -5,6 +5,9 @@ import java.io.File
 fun main() {
     val input = File("src/day15/day15.txt").readLines().first().split(",")
     println(input.sumOf { getHash(it) })
+    val boxes = getBoxes(input)
+    // println(boxes.map { "${it.boxNumber} ${it.getFocusPower()}" })
+    println(boxes.sumOf { it.getFocusPower() })
 
 }
 
@@ -16,4 +19,48 @@ fun getHash(input: String): Int {
         currentValue %= 256
     }
     return currentValue
+}
+
+data class Lens(val label: String, var focalLength: Int)
+
+class Box(val boxNumber: Int) {
+
+    private val lensList = mutableListOf<Lens>()
+
+    fun doOperation(label: String, operation: String) {
+        if ("-" == operation) {
+            lensList.removeIf { it.label == label }
+        } else if ('=' == operation.first()) {
+            val found = lensList.find { it.label == label }
+            val focalLength = operation.drop(1).toInt()
+            if (found != null) {
+                found.focalLength = focalLength
+            } else {
+                lensList.add(Lens(label, focalLength))
+            }
+        }
+    }
+
+    fun getFocusPower(): Int {
+        return lensList.mapIndexed { index, lens -> (boxNumber + 1) * (index + 1) * lens.focalLength }
+            .sum()
+    }
+}
+
+val LABEL = Regex("""(\w+)""")
+
+fun getBoxes(input: List<String>): List<Box> {
+    var boxMap = mutableMapOf<Int, Box>()
+    input.forEach {
+        val instruction = it
+        val match = LABEL.find(instruction)
+        match?.let {
+            val label = match.value
+            val boxIndex = getHash(label)
+            val box = boxMap.getOrPut(boxIndex) { Box(boxIndex) }
+            box.doOperation(label, instruction.substring(match.range.last + 1))
+        }
+
+    }
+    return boxMap.keys.sorted().map { boxMap[it]!! }
 }
--- 2023/src/day15/Day15KtTest.kt
+++ 2023/src/day15/Day15KtTest.kt
@@ -14,4 +14,14 @@ class Day15KtTest {
         val input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7".split(",")
         assertEquals(1320, input.sumOf { getHash(it) })
     }
+
+    @Test
+    fun testExampleBox() {
+        val input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7".split(",")
+        val boxes = getBoxes(input)
+        assertEquals(3, boxes.size)
+        println(boxes.map { it.getFocusPower() })
+        assertEquals(145, boxes.sumOf { it.getFocusPower() })
+    }
+
 }
