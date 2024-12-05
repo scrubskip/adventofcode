07ec8af8f4731b05d86bc1dee8997c91056b0242
Justin Koh <kip@kipster.org>
2024-12-04 20:10:01 -0800

    Day 3.2 done.

--- 2024/src/day03/Day03.kt
+++ 2024/src/day03/Day03.kt
@@ -5,11 +5,41 @@ import java.io.File
 fun main() {
     val input = File("src/day03/day03input.txt").readLines()
     println(input.sumOf { getMult(it) })
+
+    println(getMultWithDo(input.joinToString(separator = "")))
 }
 
 val MULT_REGEX = Regex("mul\\((\\d+),(\\d+)\\)")
 
+val DO_STRING = "do()"
+val DONT_STRING = "don\'t()"
+
 fun getMult(input : String) : Int {
     val matchResults = MULT_REGEX.findAll(input)
     return matchResults.map { it.groupValues[1].toInt() * it.groupValues[2].toInt() }.sum()
+}
+
+
+fun getMultWithDo(input : String) : Int {
+    var index = 0
+    var result = 0
+    while (index < input.length && index != -1) {
+        if (index > 0) {
+            index += DO_STRING.length
+        }
+        var dontIndex = input.indexOf(DONT_STRING, index)
+        if (dontIndex == -1) {
+            dontIndex = input.length - 1
+        }
+        var enabledString = input.substring(index, dontIndex)
+        // println(enabledString)
+        result += getMult(enabledString)
+        // now check the next do after don't index
+        if (dontIndex < input.length) {
+            index = input.indexOf(DO_STRING, dontIndex)
+        } else {
+            break
+        }
+    }
+    return result
 }
--- 2024/src/test/kotlin/day03/Day03KtTest.kt
+++ 2024/src/test/kotlin/day03/Day03KtTest.kt
@@ -11,4 +11,10 @@ class Day03KtTest {
         val input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
         assertEquals(161, getMult(input))
     }
+
+    @Test
+    fun testDoDontExample() {
+        val input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+        assertEquals(48, getMultWithDo(input))
+    }
 }
